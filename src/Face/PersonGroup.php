b1768ad4bcce098bba3641e83e2ca512e6c9711a
centerax <pablo@ebizmarts.com>
2017-07-20 11:44:49 -0300

    new command to Delete PersonGroup

--- src/Face/PersonGroup.php
+++ src/Face/PersonGroup.php
@@ -3,39 +3,14 @@
 namespace Ebizmarts\MsCognitiveService\Face;
 
 use Ebizmarts\MsCognitiveService\Face\Data\V1_0\PersonGroup as PersonGroupData;
-use GuzzleHttp\Client;
 
 class PersonGroup
 {
-    private $httpClient;
-
-    /**
-     * Recommendation constructor.
-     * @param string $accountKey
-     * @param string $baseUri
-     */
-    public function __construct($accountKey, $baseUri)
-    {
-        $this->httpClient = new Client(
-            [
-                "base_uri" => $baseUri,
-                "headers"  => [
-                    "Ocp-Apim-Subscription-Key" => $accountKey,
-                    "Content-Type" => "application/json; charset=utf-8"
-                ]
-            ]
-        );
-    }
-
     /**
      * @return array \Ebizmarts\MsCognitiveService\Face\Data\V1_0\PersonGroup
      */
-    public function getAllGroups()
+    public function getAllGroups($groups)
     {
-        $response = $this->httpClient->request('GET', 'persongroups');
-        $contents = $response->getBody()->getContents();
-        $groups = \GuzzleHttp\json_decode($contents);
-
         $return = [];
 
         foreach($groups as $model)
@@ -50,21 +25,5 @@ class PersonGroup
         }
 
         return $return;
-    }
-
-    public function create(PersonGroupData $personGroup)
-    {
-        $body = [
-            'json' => [
-                'name' => $personGroup->getName(),
-                'userData' => $personGroup->getUserData()
-            ]
-        ];
-
-        $putUri = 'persongroups/' . $personGroup->getPersonGroupId();
-
-        $result = $this->httpClient->put($putUri, $body);
-
-        return $result;
     }
 }
