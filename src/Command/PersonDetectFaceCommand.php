ce68b6855f2269c28e77d0742cb7b8146414bda5
centerax <pablo@ebizmarts.com>
2017-07-21 10:59:35 -0300

    DetectFace command

---
+++ src/Command/PersonDetectFaceCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Ebizmarts\MsCognitiveService\Command;
+
+use Ebizmarts\MsCognitiveService\Face\Data\V1_0\Person;
+use Ebizmarts\MsCognitiveService\Face\FaceApiManager;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class PersonAddFaceCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('face:detect')
+            ->setDescription('Detects face on image.')
+            ->setHelp('This command allows you to detect a face on an image.')
+            ->addArgument('file-path', InputArgument::REQUIRED, 'Path to file on disk.');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln([
+            'Person detect face',
+            '============',
+            '',
+        ]);
+
+        $manager = new FaceApiManager();
+        $filePath = $input->getArgument('file-path');
+
+        $fileContents = file_get_contents($filePath);
+
+        $output->writeln($manager->detectFace($fileContents));
+    }
+}
